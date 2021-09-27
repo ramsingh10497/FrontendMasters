b586f1e735b864ff2002bc9af008d23b539a799f
Ram Singh <rams48705@gmail.com>
2021-09-27 23:55:08 +0530

    lasttodaycomit

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Card } from '@mui/material';
+import { Button, Card } from '@mui/material';
 import { Box } from '@mui/system'
 import React from 'react';
 import styles from './HomePage.module.css'
@@ -13,6 +13,10 @@ const HomePage = () => {
                         <span style={{ marginLeft: '60px' }}>With in-Depth,Modern</span><br/>
                         <span>Front-end Engineering Courses</span>
                     </h1>
+                </Box>
+                <Box className={styles.fm3}>
+                    <Button variant="contained" style={{backgroundColor:'rgb(213,65,48)'}}>Browse Our Courses</Button>
+                    <Button variant="contained" style={{backgroundColor:'rgb(201,80,45)'}}>View Learning Paths</Button>
                 </Box>
             </Card>
         </Box>
