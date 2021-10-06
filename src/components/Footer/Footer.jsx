42ff32c4e49d4321cffebdf42b6898ff2c40de5d
Ram Singh <rams48705@gmail.com>
2021-10-06 22:34:22 +0530

    061021

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <Box component='div' className={styles.box}>
+                <Box component="div" className={styles.imgbox}>
+                    <img alt="logo" src="https://static.frontendmasters.com/assets/fm/js/static/frontendmasters.e1e10b58c1.svg" />
+                </Box>
+                <Box className={styles.front} component="div">
+                        <Button color="inherit">Courses</Button>
+                        <Button color="inherit">Learn</Button>
+                        <Button color="inherit">Prices</Button>
+                        <Button color="inherit">Login</Button>
+                        <Button></Button>
+                </Box>
+            </Box>
         </div>
     )
 }
